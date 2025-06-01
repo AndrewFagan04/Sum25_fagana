22837f9c0b355f52dc40d14b28fceed6c730747b
AndrewFagan04 <fagana@sunypoly.edu>
2025-05-31 23:55:37 -0400

    added read file

--- main.py
+++ main.py
@@ -1,76 +1,95 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 import datetime
+import os
 
 d = datetime.datetime.now()
 
-while True:
-    i = input("Choose which model to use; QwQ (q), DeepSeek (d), Qwen (w) or Gemma3 (g): ")
-    if(i == "q"):
-        model_name = "QwQ"
-        name = "QwQ"
-        break
-    elif(i == "g"):
-        model_name = "gemma3:12b"
-        name = "Gemma3"
-        break
-    elif(i == "d"):
-        model_name = "deepseek-r1"
-        name = "DeepSeek"
-        break
-    elif(i == "w"):
-        model_name = "qwen2.5vl:latest"
-        name = "Qwen"
-        break
-    else:
-        print("Try Again")
 
 
-template = """
-Answer the question below.
+def read_file():
+    user = input("What file would you like to learn about?: ")
 
-Today's date is {year}, {month}, {day}, {hour}, {minute}
+    #opens files
+    file = open(user, 'r')
 
-Here is the conversation history: {context}
+    #reads file
+    content = file.read()
+    print(content)
 
-Question: {question}
+    #closes file
+    file.close()
+    
+    return content
 
-"""#Dig into this website for useful information based on the question: {website}
-"""
 
-Answer:
-"""
 
-#
-#
 
-model = OllamaLLM(model=model_name)
-prompt = ChatPromptTemplate.from_template(template)
-chain = prompt | model
 
-def handle_conversation():
+def models():
+    while True:
+        i = input("Choose which model to use; QwQ (q), DeepSeek (d), Qwen (w) or Gemma3 (g): ")
+        if(i == "q"):
+            model_name = "QwQ"
+            name = "QwQ"
+            break
+        elif(i == "g"):
+            model_name = "gemma3:12b"
+            name = "Gemma3"
+            break
+        elif(i == "d"):
+            model_name = "deepseek-r1"
+            name = "DeepSeek"
+            break
+        elif(i == "w"):
+            model_name = "qwen2.5vl:latest"
+            name = "Qwen"
+            break
+        else:
+            print("Try Again")
+
+    template = """
+    Answer the question below.
+
+    Today's date is {year}, {month}, {day}, {hour}, {minute}
+
+    Here is the conversation history: {context}
+
+    Question: {question}
+
+    Use this file to help answer the question {content}
+
+    Answer:
+    """
+
+    return model_name, name, template
+
+
+
+
+
+def handle_conversation(model_name, name, template, content):
+    model = OllamaLLM(model=model_name)
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+
+
     context = ""
     print("Welcome to the AI Model ", name,"! Type 'exit' to quit.")
-    w = input("Type the website you'll like to use as your source: ")
-    
-
-
-
-
-
 
     while True:
         user_input = input("You: ")
         if user_input.lower() == "exit":
             break
 
-        result = chain.invoke({"year": d.year, "month": d.month, "day": d.day, "hour": d.hour, "minute": d.minute, "context": context, "question": user_input, "website": w})
+        result = chain.invoke({"year": d.year, "month": d.month, "day": d.day, "hour": d.hour, "minute": d.minute, "context": context, "question": user_input, "content": content})
         print(name,": ", result)
-        context += f"\nUser: {user_input}\nAI: {result}\nWeb: {w}"
+        context += f"\nUser: {user_input}\nAI: {result}\nFile: {content}"
+
+
 
 if __name__ == "__main__":
-    handle_conversation()
+    c = read_file()
+    m, n, t = models()
+    handle_conversation(m, n, t, c)
 
-#
-#
-#
